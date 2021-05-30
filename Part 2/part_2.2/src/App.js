92aa0caaa98955ebe893a77cb8f68c898b2ab25b
BodduSaiRamaKrishna <66328946+BodduSaiRamaKrishna@users.noreply.github.com>
2021-05-30 20:51:48 +0530

    Create App.js

---
+++ Part 2/part_2.2/src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import React from 'react'
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+  const part4 = 'Redux'
+  const exercises4 = 11 
+
+  return (
+    <div>
+      <h1>{course}</h1>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+      <p>
+        {part4} {exercises4}
+      </p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3 + exercises4}</p>
+    </div>
+  )
+}
+
+export default App;
