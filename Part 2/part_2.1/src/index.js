b4b8a684e1b02934f116ebabe217af04803e3371
BodduSaiRamaKrishna <66328946+BodduSaiRamaKrishna@users.noreply.github.com>
2021-05-31 22:55:28 +0530

    Update index.js

--- Part 2/part_2.1/src/index.js
+++ Part 2/part_2.1/src/index.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom'
+import App from './App.js'
+
+const notes = [
+  {
+    id: 1,
+    content: 'Fudamentals Of React',
+    important: true
+  },
+  {
+    id: 2,
+    content: 'Using Props To Pass Data',
+    important: false
+  },
+  {
+    id: 3,
+    content: 'State Of A Component',
+    important: true
+  }
+]
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App notes={notes} />,
   document.getElementById('root')
-);
+)
