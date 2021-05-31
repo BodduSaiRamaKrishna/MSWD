c6beb24a746497ef6eeb7ce66615272dbb130910
BodduSaiRamaKrishna <66328946+BodduSaiRamaKrishna@users.noreply.github.com>
2021-05-31 22:55:01 +0530

    Update App.js

--- Part 2/part_2.1/src/App.js
+++ Part 2/part_2.1/src/App.js
@@ -1,30 +1,16 @@
-import './App.css';
 import React from 'react'
 
-const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14 
+const App = (props) => {
+  const { notes } = props
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <h1>Half Stack Application Development</h1>
+        {notes[0].content}<br/>
+        {notes[1].content}<br/>
+        {notes[2].content}
     </div>
   )
 }
 
-export default App;
+export default App
