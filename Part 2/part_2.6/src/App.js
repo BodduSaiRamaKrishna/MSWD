d3fc419977645019a89b4bbcaf45c7904ac0d58e
BodduSaiRamaKrishna <66328946+BodduSaiRamaKrishna@users.noreply.github.com>
2021-06-02 01:19:57 +0530

    Update App.js

--- Part 2/part_2.6/src/App.js
+++ Part 2/part_2.6/src/App.js
@@ -4,29 +4,21 @@ import Person from './components/Person'
 const App = (props) => {
   const [ persons, setPersons ] = useState(props.Phonebokk) 
   const [newName, setNewName] = useState('')
-  const [newNumber,setNewNumber]=useState('')
 
   const addName = (event) => {
     event.preventDefault()
     const personObject = {
       name: newName,
-      number:newNumber,
       id: persons.length + 1,
     }
 
     setPersons(persons.concat(personObject))
     setNewName('')
-    setNewNumber('')
   }
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
-  }
-
-  const handleNumberChange = (event) => {
-    console.log(event.target.value)
-    setNewNumber(event.target.value)
   }
 
   return (
@@ -35,8 +27,6 @@ const App = (props) => {
         <form onSubmit={addName}>
           <div>
             Name: <input value={newName} onChange={handleNameChange}/>
-            <br/>
-            Number: <input value={newNumber} onChange={handleNumberChange}/>
           </div>
           <div>
             <button type="submit">add</button>
@@ -45,8 +35,6 @@ const App = (props) => {
         <h2>Numbers</h2>
         <div>
           {newName}
-          <br/>
-          {newNumber}
         </div>
       </div>
     )
