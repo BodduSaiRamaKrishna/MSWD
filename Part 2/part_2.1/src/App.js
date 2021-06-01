18dacaca76661988f8a35f9db708c774a858e928
BodduSaiRamaKrishna <66328946+BodduSaiRamaKrishna@users.noreply.github.com>
2021-06-01 09:29:48 +0530

    Update App.js

--- Part 2/part_2.1/src/App.js
+++ Part 2/part_2.1/src/App.js
@@ -1,16 +1,2 @@
-import React from 'react'
-
-const App = (props) => {
-  const { notes } = props
-
-  return (
-    <div>
-      <h1>Half Stack Application Development</h1>
-        {notes[0].content}:{course.parts[0].exercises}<br/>
-        {notes[1].content}:{course.parts[1].exercises}<br/>
-        {notes[2].content}:{course.parts[2].exercises}
-    </div>
-  )
-}
-
-export default App
+import ReactDOM from 'react-dom'
+import App from './App.js'
