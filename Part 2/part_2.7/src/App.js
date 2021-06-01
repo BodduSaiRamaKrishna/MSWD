368150fd2a338b8f14762d45c28a07d2da605d56
BodduSaiRamaKrishna <66328946+BodduSaiRamaKrishna@users.noreply.github.com>
2021-06-02 01:25:01 +0530

    Update App.js

--- Part 2/part_2.7/src/App.js
+++ Part 2/part_2.7/src/App.js
@@ -4,13 +4,11 @@ import Person from './components/Person'
 const App = (props) => {
   const [ persons, setPersons ] = useState(props.Phonebook) 
   const [newName, setNewName] = useState('')
-  const [newNumber,setNewNumber]=useState('')
 
   const addName = (event) => {
     event.preventDefault()
     const personObject = {
       name: newName,
-      number:newNumber,
       id: persons.length + 1,
     }
     const repeatOrNot = () => {
@@ -28,17 +26,11 @@ const App = (props) => {
     repeatOrNot()
     setPersons(persons.concat(personObject))
     setNewName('')
-    setNewNumber('')
   }
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
-  }
-
-  const handleNumberChange = (event) => {
-    console.log(event.target.value)
-    setNewNumber(event.target.value)
   }
 
   return (
@@ -47,8 +39,6 @@ const App = (props) => {
         <form onSubmit={addName}>
           <div>
             Name : <input value={newName} onChange={handleNameChange}/>
-            <br/>
-            Number : <input value={newNumber} onChange={handleNumberChange}/>
           </div>
           <div>
           <input type = "submit" text = "Add" />
@@ -56,7 +46,7 @@ const App = (props) => {
         </form>
         <h2>Numbers</h2>
         <div>
-          {newName} {newNumber}
+          {newName}
         </div>
       </div>
     )
