bd9e4fc463909c97784ae59fe586255ee776f38a
BodduSaiRamaKrishna <66328946+BodduSaiRamaKrishna@users.noreply.github.com>
2021-06-06 01:21:09 +0530

    Update mongo.js

--- Part 3/part_3.12/testnode/mongo.js
+++ Part 3/part_3.12/testnode/mongo.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-if (process.argv.length < 3) {
+if ( process.argv.length<3 ) {
   console.log('Please provide the password as an argument: node mongo.js <password>')
   process.exit(1)
 }
@@ -13,21 +13,25 @@ const url =
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
 const noteSchema = new mongoose.Schema({
-  content: String,
-  date: Date,
-  important: Boolean,
+  name: String,
+  telephone: String
 })
 
 const Note = mongoose.model('Note', noteSchema)
+
 const note = new Note({
-    content: 'HTML is Easy',
-    date: new Date(),
-    important: true,
-  })
+  name: `Ramji`,
+  telephone: 9100226132
+})
+
+note.save().then(response => {
+  console.log(`notes saved`)
+  mongoose.connection.close()
+})
 
 Note.find({}).then(result => {
     result.forEach(note => {
       console.log(note)
     })
     mongoose.connection.close()
-  })
+})
